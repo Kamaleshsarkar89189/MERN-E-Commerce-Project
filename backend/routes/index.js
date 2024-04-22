f6bf9a923a031ea22f7f08ebfeca0bc459b8defd
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2024-04-23 00:58:53 +0530

    test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,10 +7,25 @@ const userSignInContoller = require('../controller/userSignin')
 const userDetailsController = require('../controller/userDetails')
 const authToken = require('../middleware/authtoken')
 const userLogout = require('../controller/userLogout')
+const allUsers = require('../controller/allUsers')
+const updateUser = require('../controller/updateUser')
+const UploadProductController = require('../controller/uploadProduct')
+const getProductController = require('../controller/getProduct')
+const updateProductController = require('../controller/updateProduct')
 
 router.post("/signup", userSignUpController)
 router.post("/signin", userSignInContoller)
 router.get("/user-details",authToken,userDetailsController)
 router.get("/userLogout",userLogout)
+
+//admin panel
+router.get("/all-user",authToken,allUsers)
+router.post("/update-user",authToken,updateUser)
+
+//product
+router.post("/upload-product",authToken,UploadProductController)
+router.get("/get-product",getProductController)
+router.post("/update-product",authToken,updateProductController)
+
 
 module.exports = router
